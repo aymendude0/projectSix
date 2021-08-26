6a1555fb6da364037312ebace5438f8430aae97c
Aymen <aymenm@ive.com>
2021-08-25 21:31:26 -0400

    Updates

---
+++ precache-manifest.12f7276a8ce96aac3a34b383b494adff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a984f7eddc86b4d602fc08d7e1917c6a",
+    "url": "/projectSix/index.html"
+  },
+  {
+    "revision": "9fd6367cd8e6da1214c6",
+    "url": "/projectSix/static/css/main.9ae21092.chunk.css"
+  },
+  {
+    "revision": "e708e1c98f1b6bcf34cf",
+    "url": "/projectSix/static/js/2.547e7d37.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/projectSix/static/js/2.547e7d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd6367cd8e6da1214c6",
+    "url": "/projectSix/static/js/main.0630acc4.chunk.js"
+  },
+  {
+    "revision": "b59e91dfd1cbd84b2e4a",
+    "url": "/projectSix/static/js/runtime-main.15288e94.js"
+  }
+]);
